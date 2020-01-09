3e9d643ff23337b5f9bf1f4621e48770ac708ac9
Joe-Kwon <58056494+Joe-Kwon@users.noreply.github.com>
2020-01-08 21:19:00 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             // !info
             case "info":
                 bot.sendMessage({
-                    to: user,
+                    to: channelID,
                     message: "To-Do-List Bot info: \n" +
                              "Version: " + "lol jk" + "\n" +
                              "This bot does this and that"
